5ad364ac795e86fd3dc3262bf3054c5e90137c33
Eliel Raposo da silva <32067603+ElielSilva@users.noreply.github.com>
2023-10-08 11:12:05 -0300

    feat:
    feita as implementações das classes nokia e iphone

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,17 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Nokia(string numero, string modelo, string mei, int memoria) : base(numero, modelo, mei, memoria)
+        {
+
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"instalando o aplicativo {nomeApp} no nokia");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia");
+Nokia nokia = new Nokia("123456","modelo 1","11111111111",64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+Iphone iphone = new Iphone("6543211","modelo 2","2222222222",128);
+iphone.Ligar();
+iphone.InstalarAplicativo("telegram");
